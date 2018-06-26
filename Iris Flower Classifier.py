66706ee9def77619ed7b3d1d8e5c478777a467ba
Hasnain Cheena <hasnaincheena.hc@gmail.com>
2018-06-26 12:25:12 +1200

    Optimized code and added more evaluation functionality

--- Iris Flower Classifier.py
+++ Iris Flower Classifier.py
@@ -1,41 +1,40 @@
-from sklearn.datasets import load_iris
+#import modules
 from sklearn import tree
 import numpy as np
 import pandas as pd
+import random
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+from sklearn.model_selection import train_test_split  
+from sklearn.metrics import classification_report, confusion_matrix 
+
+#evaluate accuracy of the model 
+#calculate RMSE (root mean squared error) to evaluate accuracy 
+def rmse(y, yhat):
+    return np.sum((y - yhat)**2)**0.5
 
 #import iris dataset
 iris = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header = None, 
                   names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species'])
 #note: target names are the labels and the features are the actual data
 
-#split dataset into a training and test dataframe
-n = len(iris)
-is_train = np.random.rand(n) < 0.7
-train = iris[is_train].reset_index(drop=True)
-test = iris[~is_train].reset_index(drop=True)
+#split into features and labels
+data = iris.drop('species', axis = 1)
+labels = iris['species']
+#split into training and testing datasets
+train, test, train_labels, test_labels = train_test_split(data, labels, test_size = 0.20) 
 
-#split species from rest of data
-train_labels = train['species'].copy()
-train = train.drop('species', axis=1)
-test_labels = test['species'].copy()
-test = test.drop('species', axis=1)
-
+#predict species of flower depending on the sepal length, sepal width, petal length & petal width
 #create decision tree classifier
 clf = tree.DecisionTreeClassifier()
-#clf.fit(features, labels) - general form of the fit command
 clf.fit(train, train_labels)
 
-
-#use testing data to test the accuracy of the model
-print("Test Outputs")
-#output we want to see
-print(test_labels)
-
-print() #blank line 
-
+#predictions 
 output = clf.predict(test) #prediction using decision tree
-print(output)
-print('Prediction Outputs')
+
+print(confusion_matrix(test_labels, output))
+print(classification_report(test_labels, output))
+
 
 
 
